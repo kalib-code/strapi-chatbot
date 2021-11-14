5bad9008e45a09f82536662189258ed2e27746da
kalib-code <kalibuas@gmail.com>
2021-11-14 18:23:10 +0800

    initial

--- config/database.js
+++ config/database.js
@@ -12,17 +12,10 @@ module.exports = ({
         database: env('DATABASE_NAME', 'chat-bot'),
         username: env('DATABASE_USERNAME', 'api_admin'),
         password: env('DATABASE_PASSWORD', 'Saltedasin123'),
-        ssl: env.bool('DATABASE_SSL', false),
+        schema: 'public',
+        ssl: { rejectUnauthorized: false },
       },
-      options: {
-        "pool": {
-          "min": 0,
-          "max": 10,
-          "idleTimeoutMillis": 30000,
-          "createTimeoutMillis": 30000,
-          "acquireTimeoutMillis": 30000
-        }
-      }
+      options: {}
     },
   },
 });
