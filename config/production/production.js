9487255fc4f385ba76fe341251550fb571a8be58
kalib-code <kalibuas@gmail.com>
2021-11-14 11:43:15 +0800

    set up

--- config/production/production.js
+++ config/production/production.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => ({
         username: config.user,
         password: config.password,
         ssl: {
-          rejectUnauthorized: true,
+          rejectUnauthorized: false,
         },
       },
       options: {}
