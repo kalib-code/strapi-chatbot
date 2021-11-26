5009be430bb30bde4919972764913d3c20f82592
kalib-code <kalibuas@gmail.com>
2021-11-26 13:47:24 +0800

    database production

--- config/database.js
+++ config/database.js
@@ -1,26 +1,23 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({
-  env
-}) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'chat-bot'),
-        username: env('DATABASE_USERNAME', 'api_admin'),
-        password: env('DATABASE_PASSWORD', 'Saltedasin123')
-      },
-      options: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: {
-          ca: env('DATABASE_CA', ""),
+          rejectUnauthorized: false,
         },
       },
+      options: {},
     },
   },
 });
